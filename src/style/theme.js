8317f8d1bdd7d36f5d9b05ed0ec65aab67441b7e
rachelskim <loafcheck@gmail.com>
2022-12-15 10:12:55 +0900

    :art: style: create global theme

---
+++ src/style/theme.js
@@ -0,0 +1,22 @@
+const commonTheme = {
+    snBlue: "#33AFD8",
+    snYellow: "#F7CE15", 
+    snDisabled: "#85CFE8",
+    snGrey: "#85CFE8",
+    snBlack: "#000000",
+    snRed:"#EB5757",
+    snWhite:"#FFFFFF",
+
+  };
+
+//   what is this? mode?
+  const light = {
+    ...commonTheme,
+  };
+  //   what is this? mode?
+  const dark = {
+    ...commonTheme,
+  };
+  
+  export { light, dark };
+  
