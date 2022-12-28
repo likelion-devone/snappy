c468feea0443d89294ae30c31dca0b4622a3059c
Rachel S Kim / 김민정 <loafcheck@gmail.com>
2022-12-28 17:22:30 +0900

    ✨ feat: 404 페이지 UI 구현, 이전페이지 버튼 기능추가

--- src/page/404/index.jsx
+++ src/page/404/index.jsx
@@ -1,5 +1,10 @@
 import SnappyError from "asset/logo-404-343264.png";
+import { BUTTON_STATE } from "constant/button_state";
+import { BUTTON_SIZE } from "constant/size";
 import styled from "styled-components";
+import Button from "component/common/Button";
+import { useNavigate } from "react-router-dom";
+import { FONT_SIZE } from "constant/style";
 
 const Wrapper = styled.div`
   display: grid;
@@ -14,14 +19,39 @@ const Wrapper = styled.div`
     grid-row-end: 2;
     transform: translate(0, 100px);
   }
-  .mainlogo {
+  .PrevPage {
     grid-column-start: 2;
     grid-column-end: 3;
     grid-row-start: 2;
     grid-row-end: 3;
     z-index: 1;
     transform: scale(1.5);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
+  Button {
+    position: relative;
+    left: 35px;
+  }
+  .PageNotFound {
+    position: relative;
+    left: 35px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    font-size: ${FONT_SIZE.LARGE};
+    color: ${(props) => props.theme.snGreyMain};
+    font-weight: 700;
+  }
+
+  /* .mainlogo {
+    grid-column-start: 2;
+    grid-column-end: 3;
+    grid-row-start: 2;
+    grid-row-end: 3;
+    z-index: 1;
+    transform: scale(1.5);
+  } */
   .logo3 {
     grid-column-start: 1;
     grid-column-end: 2;
@@ -36,12 +66,38 @@ const Wrapper = styled.div`
     transform: translate(0, 100px);
   }
 `;
+
+// const PrevPage = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `;
 export default function NotFoundErrorPage() {
+  const navigate = useNavigate();
+  const goback = () => {
+    navigate(-1);
+  };
   return (
     <Wrapper>
       <img src={SnappyError} alt="404 로고 이미지입니다" className="logo1" />
       <img src={SnappyError} alt="404 로고 이미지입니다" className="logo2" />
-      <img src={SnappyError} alt="404 로고 이미지입니다" className="mainlogo" />
+      <div className="PrevPage">
+        <img
+          src={SnappyError}
+          alt="404 로고 이미지입니다"
+          className="mainlogo"
+        />
+        <p className="PageNotFound">Page Not Found</p>
+        <Button
+          className="PrevPageBtn"
+          type="button"
+          size={BUTTON_SIZE.LARGE_44}
+          state={BUTTON_STATE.LARGE_44.ABLED}
+          onClick={goback}
+        >
+          이전 페이지
+        </Button>
+      </div>
+
       <img src={SnappyError} alt="404 로고 이미지입니다" className="logo3" />
       <img src={SnappyError} alt="404 로고 이미지입니다" className="logo4" />
     </Wrapper>
