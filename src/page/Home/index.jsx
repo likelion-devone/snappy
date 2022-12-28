014c57e7f9901d4b5c0618f5b38a1dea04640d0f
shiwoo.park <custardcream@kakao.com>
2022-12-28 12:13:36 +0900

    🌱 chore: HomePage log 제거

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,14 +1,9 @@
-import { useEffect } from "react";
 import PostCard from "component/common/PostCard/index";
 import useFetch from "hook/useFetch";
 import { req } from "lib/api";
 
 export default function HomePage() {
   const [isPostDataLoading, postData, _error] = useFetch(req.post.feed);
-
-  useEffect(() => {
-    console.log(postData);
-  }, [postData]);
 
   if (isPostDataLoading || !postData) {
     return <>로딩중</>;
