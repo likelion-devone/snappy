5996a26bb29237b39921d727cf3c43bd002c98d4
chaeryun0 <chaeryun0@gmail.com>
2022-12-29 15:04:40 +0900

    ✨ feat: 내 게시글도 피드에 보이도록 기능 구현

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
 import PostCard from "component/common/PostCard/index";
 import useFetch from "hook/useFetch";
 import { req } from "lib/api";
+import useAuthInfo from "hook/useAuthInfo";
 
 export default function HomePage() {
+  const authInfo = useAuthInfo();
+
   const [isPostDataLoading, postData, _error] = useFetch(req.post.feed);
 
-  useEffect(() => {
-    console.log(postData);
-  }, [postData]);
+  // 내 게시글도 피드에 보이게 하기
+  const [isMyPostDataLoading, myPostData, __error] = useFetch(
+    req.post.userposts,
+    { accountname: authInfo.accountname }
+  );
 
-  if (isPostDataLoading || !postData) {
+  // 로딩중이면 데이터가 들어오지 않습니다.
+  if (isPostDataLoading || !postData || isMyPostDataLoading || !myPostData) {
     return <>로딩중</>;
   }
 
