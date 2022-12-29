9c357bccce50a5e9dde25bd623288381ddf530cd
shiwoo.park <custardcream@kakao.com>
2022-12-29 11:33:10 +0900

    🐛 fix: 404 레이아웃 개선

--- src/page/404/index.jsx
+++ src/page/404/index.jsx
@@ -6,52 +6,54 @@ import Button from "component/common/Button";
 import { useNavigate } from "react-router-dom";
 import { FONT_SIZE } from "constant/style";
 
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 1.5fr 1fr;
-  grid-template-rows: 1fr 1.5fr 1fr;
-  align-items: center;
+const MainLogoWrapper = styled.div`
+  position: absolute;
+  inset: 0;
 
-  .logo2 {
-    grid-column-start: 3;
-    grid-column-end: 4;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    transform: translate(0, 100px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  
+  img {
+    width: 50vw;
+    max-width: 500px;
+    transform: translateX(max(-4vw, -45px));
   }
-  .PrevPage {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 2;
-    grid-row-end: 3;
-    z-index: 1;
-    transform: scale(1.5);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  Button {
-    position: relative;
-    left: 35px;
-  }
+
   .PageNotFound {
-    position: relative;
-    left: 35px;
     padding-top: 20px;
     padding-bottom: 20px;
     font-size: ${FONT_SIZE.LARGE};
     color: ${(props) => props.theme.snGreyMain};
     font-weight: 700;
   }
+`
 
-  /* .mainlogo {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 2;
-    grid-row-end: 3;
-    z-index: 1;
-    transform: scale(1.5);
-  } */
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  img {
+    width: 30vw;
+    max-width: 330px;
+    transform: translateX(10vw);
+  }
+
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  align-items: center;
+  text-align: center;
+
+  .logo2 {
+    grid-column-start: 3;
+    grid-column-end: 4;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    transform: translateY(10vw);
+  }
+
   .logo3 {
     grid-column-start: 1;
     grid-column-end: 2;
@@ -63,24 +65,24 @@ const Wrapper = styled.div`
     grid-column-end: 4;
     grid-row-start: 3;
     grid-row-end: 4;
-    transform: translate(0, 100px);
+    transform: translateY(10vw);
   }
 `;
 
-// const PrevPage = styled.div`
-//   display: flex;
-//   flex-direction: column;
-// `;
 export default function NotFoundErrorPage() {
   const navigate = useNavigate();
   const goback = () => {
     navigate(-1);
   };
   return (
-    <Wrapper>
-      <img src={SnappyError} alt="404 로고 이미지입니다" className="logo1" />
-      <img src={SnappyError} alt="404 로고 이미지입니다" className="logo2" />
-      <div className="PrevPage">
+    <>
+      <Wrapper>
+        <img src={SnappyError} alt="404 로고 이미지입니다" className="logo1" />
+        <img src={SnappyError} alt="404 로고 이미지입니다" className="logo2" />
+        <img src={SnappyError} alt="404 로고 이미지입니다" className="logo3" />
+        <img src={SnappyError} alt="404 로고 이미지입니다" className="logo4" />
+      </Wrapper>
+      <MainLogoWrapper>
         <img
           src={SnappyError}
           alt="404 로고 이미지입니다"
@@ -96,10 +98,7 @@ export default function NotFoundErrorPage() {
         >
           이전 페이지
         </Button>
-      </div>
-
-      <img src={SnappyError} alt="404 로고 이미지입니다" className="logo3" />
-      <img src={SnappyError} alt="404 로고 이미지입니다" className="logo4" />
-    </Wrapper>
+      </MainLogoWrapper>
+    </>
   );
 }
