9329b502455ef89bd1df5f84e41aae2f33358cf2
shiwoo.park <custardcream@kakao.com>
2022-12-30 12:45:36 +0900

    ✨ feat: 프로필 페이지 TopNav 연결, 로그아웃 구현

--- src/page/Profile/index.jsx
+++ src/page/Profile/index.jsx
@@ -1,10 +1,44 @@
-import useAuthInfo from "hook/useAuthInfo";
+import { useContext } from "react";
+
 import PageDetails from "component/Profile/PageDetails/index";
+import { TopNavElement } from "component/common/Navbar/TopNav/index";
+import { AlertModal, DropdownModal } from "component/common/Modal";
+
+import { AuthContext } from "lib/auth/AuthProvider"
+
+import useAuthInfo from "hook/useAuthInfo";
+import useTopNavSetter from "hook/useTopNavSetter";
+import useDropdownModal from "hook/useDropdownModal";
+import useModal from "hook/useModal";
 
 function ProfilePage() {
   const { accountname } = useAuthInfo();
+  const { handleLogout } = useContext(AuthContext);
+  const [isProfileMoreModalOpened, openProfileMoreModal, closeProfileMoreModal] = useDropdownModal();
+  const [isLogoutAlertModalOpened, openLogoutAlertModal, closeLogoutAlertModal, confirmLogoutAlertModal] = useModal(handleLogout);
 
-  return <PageDetails accountname={accountname} $isMyProfile={true} />
+  useTopNavSetter({
+    left: <TopNavElement.GoBackButton />,
+    right: (
+      <TopNavElement.MoreButton onClick={openProfileMoreModal} />
+    )
+  })
+
+  return (
+    <>
+      <PageDetails accountname={accountname} $isMyProfile={true} />
+      <DropdownModal dropDown={closeProfileMoreModal} isDroppedUp={isProfileMoreModalOpened}>
+        <DropdownModal.Button onClick={openLogoutAlertModal}>
+          로그아웃
+        </DropdownModal.Button>
+      </DropdownModal>
+      <AlertModal isModalOpened={isLogoutAlertModalOpened}>
+        <AlertModal.Content>로그아웃 하시겠스내피? 📷</AlertModal.Content>
+        <AlertModal.Cancle handleModalButton={closeLogoutAlertModal}>취소</AlertModal.Cancle>
+        <AlertModal.ConfirmButton handleModalButton={confirmLogoutAlertModal}>로그아웃</AlertModal.ConfirmButton>
+      </AlertModal>
+    </>
+  )
 }
 
 export default ProfilePage;
