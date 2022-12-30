57c2323c48a3e5a9401381b27fa1cde725a3f85c
Kihyun Ryu <khryu0610@gmail.com>
2022-12-30 18:13:19 +0900

    ♻️ refactor: 게시물 등록 혹은 수정 시 바로 이미지 업로드 가능하도록 수정

--- src/component/Post/LabelImgUpload/index.jsx
+++ src/component/Post/LabelImgUpload/index.jsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 import Icons from "asset/icon/icons";
-import { useContext } from "react";
-
-import { IsUploadPossibleContext } from "../IsUploadPossibleProvider/index";
 
 // 업로드 파일 Svg
 const SvgUploadFile = styled(Icons.Image)`
@@ -11,24 +8,18 @@ const SvgUploadFile = styled(Icons.Image)`
   bottom: 76px;
   right: 16px;
   cursor: pointer;
-  
+
   width: 50px;
   height: 50px;
   border-radius: 50%;
   padding: 12px;
-  background-color: ${({ $isPossibleToUpload, theme }) =>
-    !$isPossibleToUpload ? theme.snDisabled : theme.snBlue};
-    
-  ${({ $isPossibleToUpload }) =>
-    !$isPossibleToUpload && "pointer-events: none;"}
+  background-color: ${({ theme }) => theme.snBlue};
 `;
 
 export default function LabelImgUpload() {
-  const { isPossibleToUpload } = useContext(IsUploadPossibleContext);
-
   return (
     <label htmlFor="imgUpload">
-      <SvgUploadFile title="이미지 파일 업로드" $isPossibleToUpload={isPossibleToUpload} />
+      <SvgUploadFile title="이미지 파일 업로드" />
     </label>
   );
 }
