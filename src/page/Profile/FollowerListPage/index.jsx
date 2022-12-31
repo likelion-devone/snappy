507e68a84ec9ff7ed176a906180b4d5e07c49f72
shiwoo.park <custardcream@kakao.com>
2022-12-31 16:20:09 +0900

    ♻️ refactor: FollowButton 컴포넌트 분리 및 로직 개선

--- src/page/Profile/FollowerListPage/index.jsx
+++ src/page/Profile/FollowerListPage/index.jsx
@@ -1,114 +1,16 @@
-import styled from "styled-components";
 import { useParams } from "react-router-dom";
-import PropTypes from "prop-types";
-import useAPI from "hook/useAPI";
-import { useEffect, useState } from "react";
+
+import FollowerList from "component/Profile/FollowerList/index";
+
+import useFetch from "hook/useFetch";
+
 import { req } from "lib/api/index";
-
-import Button from "component/common/Button/index";
-import SmallProfile from "component/common/SmallProfile/index";
-
-import { BUTTON_STATE } from "constant/button_state";
-import { BUTTON_SIZE, PROFILE_SIZE } from "constant/size";
-import routeResolver from "util/routeResolver";
-import ROUTE from "constant/route";
-
-const FollowerList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-
-  margin-top: 14px;
-`;
 
 export default function FollowerListPage() {
   const { accountname } = useParams();
 
-  const [isFollowerLoading, followerData, _followerError, getFollowerData] =
-    useAPI(req.profile.follower);
+  const [isFollowerLoading, followerData, _followerError] =
+    useFetch(req.profile.follower, { accountname });
 
-  const [_isUnfollowLoading, _unfollowResponse, _unfollowError, unfollow] =
-    useAPI(req.profile.unfollow);
-
-  const [_isFollowLoading, _followResponse, _followError, follow] = useAPI(
-    req.profile.follow
-  );
-
-  useEffect(() => {
-    getFollowerData({ accountname });
-  }, [getFollowerData, accountname]);
-
-  async function handleFollowButton(isFollow, targetAccountname) {
-    if (isFollow) {
-      await unfollow({ accountname: targetAccountname });
-    } else {
-      await follow({ accountname: targetAccountname });
-    }
-    return;
-  }
-
-  return isFollowerLoading || !followerData ? (
-    "로딩중"
-  ) : (
-    <FollowerList>
-      {followerData.map((follower) => {
-        return (
-          <li key={follower._id}>
-            <SmallProfile
-              size={PROFILE_SIZE.MEDIUM}
-              src={follower.image}
-              imageTo={routeResolver(ROUTE.PROFILE, follower.accountname)}
-            >
-              <SmallProfile.Side
-                left={
-                  <SmallProfile.Side.Title
-                    title={follower.username}
-                    subtitle={follower.intro}
-                    gap={6}
-                    titleTo={routeResolver(ROUTE.PROFILE, follower.accountname)}
-                  />
-                }
-                right={
-                  <FollowButton
-                    initialIsFollowing={follower.isfollow}
-                    accountname={follower.accountname}
-                    handleFollowButton={handleFollowButton}
-                  />}
-              />
-            </SmallProfile>
-          </li>
-        );
-      })}
-    </FollowerList>
-  );
-}
-
-function FollowButton({ initialIsFollowing, accountname, handleFollowButton }) {
-  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
-  // Single Source Of Truth
-  return (
-    <Button
-      size={BUTTON_SIZE.X_SMALL}
-      state={
-        isFollowing
-          ? BUTTON_STATE.X_SMALL.ACTIVATED
-          : BUTTON_STATE.X_SMALL.ABLED
-      }
-      onClick={() => {
-        handleFollowButton(
-          isFollowing,
-          accountname
-        );
-        setIsFollowing((prev) => !prev)
-      }}
-    >
-      {isFollowing ? "취소" : "팔로우"}
-    </Button>
-  )
-}
-
-FollowButton.propTypes = {
-  initialIsFollowing: PropTypes.bool.isRequired,
-  accountname: PropTypes.string.isRequired,
-  handleFollowButton: PropTypes.func.isRequired
+  return <FollowerList $isLoading={isFollowerLoading} data={followerData} />
 }
--- src/page/Profile/FollowingListPage/index.jsx
+++ src/page/Profile/FollowingListPage/index.jsx
@@ -1,101 +1,15 @@
-import styled from "styled-components";
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import useAPI from "hook/useAPI";
+import useFetch from "hook/useFetch";
 
-import Button from "component/common/Button/index";
-import SmallProfile from "component/common/SmallProfile/index";
-
-import { BUTTON_STATE } from "constant/button_state";
-import { BUTTON_SIZE, PROFILE_SIZE } from "constant/size";
-import ROUTE from "constant/route";
-
-import routeResolver from "util/routeResolver";
 import { req } from "lib/api/index";
-
-const FollowerList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-
-  margin-top: 14px;
-`;
+import FollowerList from "component/Profile/FollowerList/index";
 
 export default function FollowingListPage() {
   const { accountname } = useParams();
 
-  const [isFollowingLoading, followingData, _followingError, getFollowingData] =
-    useAPI(req.profile.following);
+  const [isFollowingLoading, followingData, _followingError] =
+    useFetch(req.profile.following, { accountname });
 
-  const [_isUnfollowLoading, _unfollowResponse, _unfollowError, unfollow] =
-    useAPI(req.profile.unfollow);
-
-  const [_isFollowLoading, _followResponse, _followError, follow] = useAPI(
-    req.profile.follow
-  );
-
-  useEffect(() => {
-    getFollowingData({ accountname });
-  }, [accountname, getFollowingData]);
-
-  async function handleFollowButton(isFollow, targetAccountname) {
-    if (isFollow) {
-      await unfollow({ accountname: targetAccountname });
-    } else {
-      await follow({ accountname: targetAccountname });
-    }
-    getFollowingData({ accountname }), 5000;
-    return;
-  }
-
-  return isFollowingLoading || !followingData ? (
-    "로딩중"
-  ) : (
-    <FollowerList>
-      {followingData.map((following) => {
-        return (
-          <li key={following._id}>
-            <SmallProfile
-              size={PROFILE_SIZE.MEDIUM}
-              src={following.image}
-              imageTo={routeResolver(ROUTE.PROFILE, following.accountname)}
-            >
-              <SmallProfile.Side
-                left={
-                  <SmallProfile.Side.Title
-                    title={following.username}
-                    subtitle={following.intro}
-                    gap={6}
-                    titleTo={routeResolver(
-                      ROUTE.PROFILE,
-                      following.accountname
-                    )}
-                  />
-                }
-                right={
-                  <Button
-                    size={BUTTON_SIZE.X_SMALL}
-                    state={
-                      following.isfollow
-                        ? BUTTON_STATE.X_SMALL.ACTIVATED
-                        : BUTTON_STATE.X_SMALL.ABLED
-                    }
-                    onClick={() => {
-                      handleFollowButton(
-                        following.isfollow,
-                        following.accountname
-                      );
-                    }}
-                  >
-                    {following.isfollow ? "취소" : "팔로우"}
-                  </Button>
-                }
-              />
-            </SmallProfile>
-          </li>
-        );
-      })}
-    </FollowerList>
-  );
+  return <FollowerList $isLoading={isFollowingLoading} data={followingData} />
 }
