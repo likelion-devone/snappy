411b19f7202f35dab22f61247c61f6ac6e2e7057
shiwoo.park <custardcream@kakao.com>
2022-12-31 23:26:20 +0900

    ✨ feat: Follower / Following List Page TopNav 연결

--- src/page/Profile/FollowerListPage/index.jsx
+++ src/page/Profile/FollowerListPage/index.jsx
@@ -1,13 +1,27 @@
 import { useParams } from "react-router-dom";
 
 import FollowerList from "component/Profile/FollowerList/index";
+import { TopNavElement } from "component/common/Navbar/TopNav/index";
 
+import useTopNavSetter from "hook/useTopNavSetter";
 import useFetch from "hook/useFetch";
 
 import { req } from "lib/api/index";
 
 export default function FollowerListPage() {
   const { accountname } = useParams();
+
+  useTopNavSetter({
+    left: (
+      <>
+        <TopNavElement.GoBackButton />
+        <TopNavElement.Title>
+          Followers
+        </TopNavElement.Title>
+      </>
+    ),
+    right: <></>
+  })
 
   const [isFollowerLoading, followerData, _followerError] =
     useFetch(req.profile.follower, { accountname });
--- src/page/Profile/FollowingListPage/index.jsx
+++ src/page/Profile/FollowingListPage/index.jsx
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom";
 
+import FollowerList from "component/Profile/FollowerList/index";
+import { TopNavElement } from "component/common/Navbar/TopNav/index";
+
+import useTopNavSetter from "hook/useTopNavSetter";
 import useFetch from "hook/useFetch";
 
 import { req } from "lib/api/index";
-import FollowerList from "component/Profile/FollowerList/index";
 
 export default function FollowingListPage() {
   const { accountname } = useParams();
+
+  useTopNavSetter({
+    left: (
+      <>
+        <TopNavElement.GoBackButton />
+        <TopNavElement.Title>
+          Followers
+        </TopNavElement.Title>
+      </>
+    ),
+    right: <></>
+  })
 
   const [isFollowingLoading, followingData, _followingError] =
     useFetch(req.profile.following, { accountname });
