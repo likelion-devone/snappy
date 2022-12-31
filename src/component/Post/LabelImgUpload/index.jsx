f34f919b66e3fbe8279d501cf469eb3fc0f0f3f5
shiwoo.park <custardcream@kakao.com>
2022-12-31 12:39:52 +0900

    🎨 style: LabelImgUpload position 변경

--- src/component/Post/LabelImgUpload/index.jsx
+++ src/component/Post/LabelImgUpload/index.jsx
@@ -4,8 +4,7 @@ import Icons from "asset/icon/icons";
 // 업로드 파일 Svg
 const SvgUploadFile = styled(Icons.Image)`
   position: fixed;
-  // 하단 Nav로 인해 이미지가 잘려서 임의로 위치 지정, 추후 수정하겠습니다. (기존 16px)
-  bottom: 76px;
+  bottom: 16px;
   right: 16px;
   cursor: pointer;
 
