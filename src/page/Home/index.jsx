987aec295c581579c01dd718ccb8de5a404d0f92
shiwoo.park <custardcream@kakao.com>
2022-12-31 23:06:56 +0900

    ✨ feat: 팔로우 없는 경우 안내 문구

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,12 +1,44 @@
+import styled from "styled-components";
 import { useState, useContext, useEffect } from "react";
 
 import { TopNavElement } from "component/common/Navbar/TopNav/index";
 import PostCard from "component/common/PostCard/index";
 import SearchBar from "component/Home/SearchBar/index";
+import Button from "component/common/Button/index";
+import { PostDataContext } from "component/common/PostDataProvider/index";
 
 import useTopNavSetter from "hook/useTopNavSetter";
 
-import { PostDataContext } from "component/common/PostDataProvider/index";
+import { FONT_SIZE, GLOBAL_NAVBAR_HEIGHT, TOP_NAVBAR_HEIGHT } from "constant/style";
+import { BUTTON_SIZE } from "constant/size";
+import { BUTTON_STATE } from "constant/button_state";
+
+import LogoBw from "asset/logo-bw-212262.png";
+
+const NoFollowingsWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100%;
+  height: calc(100vh - ${GLOBAL_NAVBAR_HEIGHT} - ${TOP_NAVBAR_HEIGHT});
+
+  .logo {
+    width: 150px;
+    vertical-align: top;
+    transform: translate(-10px);
+  }
+  .guide {
+    margin: 23px 0;
+
+    font-weight: 400;
+    font-size: ${FONT_SIZE.BASE};
+    line-height: 14px;
+
+    color: ${({ theme }) => theme.snGreyIcon};
+  }
+`
 
 export default function HomePage() {
   const [isSearchOpened, setIsSearchOpened] = useState(false);
@@ -45,31 +77,48 @@ export default function HomePage() {
     return <>로딩중</>;
   }
 
+  const postDataSorted = [...postData, ...myPostData]
+    .sort(
+      (post1, post2) =>
+        Date.parse(post2.createdAt) -
+        Date.parse(post1.createdAt)
+    );
+
   return (
     <>
       <SearchBar handleClose={toggleSearch} $isSearchOpened={isSearchOpened} />
-      {[...postData, ...myPostData]
-        .sort(
-          (post1, post2) =>
-            Date.parse(post2.createdAt) -
-            Date.parse(post1.createdAt)
-        )
-        .map((postCard) => (
-          <PostCard
-            key={postCard.id}
-            authorId={postCard.author._id}
-            username={postCard.author.username}
-            accountname={postCard.author.accountname}
-            profileImage={postCard.author.image}
-            postId={postCard.id}
-            content={postCard.content}
-            image={postCard.image}
-            createdAt={postCard.createdAt}
-            hearted={postCard.hearted}
-            heartCount={postCard.heartCount}
-            commentCount={postCard.commentCount}
-          />
-        ))}
+      {
+        postDataSorted.length !== 0 ?
+          postDataSorted.map((postCard) => (
+            <PostCard
+              key={postCard.id}
+              authorId={postCard.author._id}
+              username={postCard.author.username}
+              accountname={postCard.author.accountname}
+              profileImage={postCard.author.image}
+              postId={postCard.id}
+              content={postCard.content}
+              image={postCard.image}
+              createdAt={postCard.createdAt}
+              hearted={postCard.hearted}
+              heartCount={postCard.heartCount}
+              commentCount={postCard.commentCount}
+            />
+          ))
+          :
+          <NoFollowingsWrapper>
+            <h2 className="sr-only">유저 팔로우 없음 안내</h2>
+            <img className="logo" src={LogoBw} alt="스내피 로고" />
+            <p className="guide">유저를 검색해 팔로우 해보세요!</p>
+            <Button
+              size={BUTTON_SIZE.LARGE_44}
+              state={BUTTON_STATE.LARGE_44.ABLED}
+              onClick={toggleSearch}
+            >
+              검색하기
+            </Button>
+          </NoFollowingsWrapper>
+      }
     </>
   )
 }
